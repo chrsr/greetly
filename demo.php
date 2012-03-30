52d78463046d80c7be7c667369d5115f6a57e2e9
Chris Roberts <chris@chrsr.com>
2012-03-31 07:46:02 +1000

    Add stats

--- demo.php
+++ demo.php
@@ -15,7 +15,7 @@
     $message .= '<table style="border-collapse:collapse; background-color:#fff;" cellpadding="0" cellspacing="0" border="0" width="100%">';
     $message .= '<tr>';
     $message .= '<td align="center">';
-    $message .= '<table style="border-collapse:collapse;" cellpadding="0" cellspacing="0" border="0" width="600">';
+    $message .= '<table style="border-collapse:collapse;" cellpadding="0" cellspacing="0" border="0" width="100%">';
     $message .= '<tr>';
     $message .= '<td height="150" align="center">';
     $message .= '<h1>Hey, good looking!</h1>';
@@ -36,4 +36,28 @@
 	mail($to, $subject, $message, $headers);
 ?>
 
-<h1>Check your email !</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>greetly ...</title>
+    <meta name="description" content="gravatar images in your email" />
+
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-5479657-10']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+</head>
+
+<body>
+
+    <h1>Check your email !</h1>
+
+</body>
+</html>
